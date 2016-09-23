c1751880238692a21fafaee348767eadee1940d8
Ryan Kurte <ryankurte@gmail.com>
2016-09-23 23:04:11 +1200

    Cleaning things up

--- graph.js
+++ graph.js
@@ -2,7 +2,7 @@
 
 console.log('Loading JS');
 
-
+// Parse hash and load convenience URLs
 $(function onReady() {
     setTimeout(function loadHashIfSpecified() {
         if(window.location.hash !== "") {
@@ -10,11 +10,11 @@ $(function onReady() {
             $('#key').val(key)
             generatePlot();
         }
-    });
+    }, 100);
 });
 
+// Process Phant data to highchart types
 function processData(dataIn) {
-
     dataIn = dataIn.reverse();
 
     // Flatten data
@@ -66,82 +66,116 @@ function processData(dataIn) {
     return data;
 }
 
-var chartData = {};
+// Display data
+function handleData(data, status) {
+
+    $("#loading").toggleClass('hidden', true);
+    $("#plotDiv").toggleClass('hidden', false);
+
+    if(status !== 'success') {
+        return handleError(status);
+    }
+
+    $('#result').toggleClass('hidden', false);
+    $('#result').toggleClass('alert-success', false);
+    $('#result').toggleClass('alert-danger', false);
+    $('#result').append('<p>Bookmark this chart <a href=./#' + key + '>here</a></p>');
+
+    var chartData = processData(data);
+
+    let chart = $('#plot').highcharts({
+        chart: {
+            type: 'spline',
+            zoomType: 'x'
+        },
+        title: {
+            text: 'Phantvis'
+        },
+        xAxis: {
+            type: 'datetime'
+        },
+        yAxis: chartData.yAxis,
+        series: chartData.datasets,
+        tooltip: {
+            shared: true
+        },
+        rangeSelector: {
+            enabled: true,
+            buttons: [{
+                type: 'hour',
+                count: 1,
+                text: '1h'
+            }, {
+                type: 'day',
+                count: 1,
+                text: '1d'
+            }, {
+                type: 'week',
+                count: 1,
+                text: '1w'
+            }, {
+                type: 'month',
+                count: 1,
+                text: '1m'
+            }, {
+                type: 'ytd',
+                text: 'YTD'
+            }, {
+                type: 'year',
+                count: 1,
+                text: '1y'
+            }, {
+                type: 'all',
+                text: 'All'
+            }]
+        }
+    });
+}
+
+// Display an error alert with the provided message
+function handleError(error) {
+    $('#loading').toggleClass('hidden', true);
+    $('#result').toggleClass('hidden', false);
+    $('#result').toggleClass('alert-success', false);
+    $('#result').toggleClass('alert-danger', true);
+    $('#result').append('<p>error :' + error + ' fetching data</p>');
+}
 
 // Form logic
 var generatePlot = function generatePlot(e) {
     if(typeof e !== 'undefined') e.preventDefault();
+    
+    // Fetch key and push to window hash
     var key = $('#key').val()
     window.location.hash = '#' + key;
 
-    var $result = $("#result");
-    $result.toggleClass('hidden', true);
+    // Hide result
+    $('#result').toggleClass('hidden', true);
 
+    // Display loading animation
     $("#loading").toggleClass('hidden', false);
 
-    $.getJSON('https://data.sparkfun.com/output/' + key + '.json', function(data, status) {
-        $("#loading").toggleClass('hidden', true);
-        $("#plotDiv").toggleClass('hidden', false);
+    // Fetch JSON data
+    let url = 'https://data.sparkfun.com/output/' + key + '.json';
 
-        chartData = processData(data);
-
-        let chart = $('#plot').highcharts({
-            chart: {
-                type: 'spline',
-                zoomType: 'x'
-            },
-            title: {
-                text: 'Phantvis'
-            },
-            xAxis: {
-                type: 'datetime'
-            },
-            yAxis: chartData.yAxis,
-            series: chartData.datasets,
-            tooltip: {
-                shared: true
-            },
-            rangeSelector: {
-                enabled: true,
-                buttons: [{
-                    type: 'hour',
-                    count: 1,
-                    text: '1h'
-                }, {
-                    type: 'day',
-                    count: 1,
-                    text: '1d'
-                }, {
-                    type: 'week',
-                    count: 1,
-                    text: '1w'
-                }, {
-                    type: 'month',
-                    count: 1,
-                    text: '1m'
-                }, {
-                    type: 'ytd',
-                    text: 'YTD'
-                }, {
-                    type: 'year',
-                    count: 1,
-                    text: '1y'
-                }, {
-                    type: 'all',
-                    text: 'All'
-                }]
-            }
-        });
-
-        function SetZoom(start, end) {
-            chart.xAxis[0].setExtremes(
-                new Date(start),
-                new Date(end)
-            );
-        }
-
+    $.getJSON(url, handleData)
+    .fail(function(error) {
+        console.log(error);
+        handleError(error.statusText)
     });
 }
 
+// Bind plot to form submission
 $("#graphForm").submit(generatePlot);
+
+
+// Google tracking
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-84661228-1', 'auto');
+ga('send', 'pageview');
+
 
